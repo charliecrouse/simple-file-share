0adf77827c20a01a9b40f48f9049be871983ff82
Charlie Crouse <charlietcrouse@icloud.com>
2020-04-17 00:20:24 -0400

    feature: enable file storage over api

--- src/routes/files.ts
+++ src/routes/files.ts
@@ -1,15 +1,19 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import { downloadFile, uploadFile } from '../utils/filestore';
 
 import { fileService } from '../services';
 
-export async function createFile(_req: Request, res: Response, next: NextFunction): Promise<void> {
+export async function createFile(req: Request, res: Response, next: NextFunction): Promise<void> {
   try {
     const file = await fileService.createFile();
+
     const payload = {
       statusCode: 201,
-      message: 'Successfully created new file',
+      message: 'The given file is being stored.',
       file,
     };
+
+    uploadFile(file.id, req);
     res.status(payload.statusCode).json(payload);
   } catch (err) {
     return next(err);
@@ -21,12 +25,7 @@ export async function readFile(req: Request, res: Response, next: NextFunction):
 
   try {
     const file = await fileService.findFileById(id);
-    const payload = {
-      statusCode: 200,
-      message: 'Successfully retrieved file',
-      file,
-    };
-    res.status(payload.statusCode).json(payload);
+    await downloadFile(file.id, res);
   } catch (err) {
     return next(err);
   }
--- src/utils/filestore.ts
+++ src/utils/filestore.ts
@@ -1,24 +1,34 @@
-import { Stream } from 'stream';
+import { Stream, Writable } from 'stream';
 import { FilestoreFactory } from '../loaders';
 
 export const ROOT_BUCKET = 'simple-file-share';
-export const FILE_BUCKET = ROOT_BUCKET + '/files';
 
-export function readFile(fileId: string): Promise<Stream> {
+export function downloadFile(fileId: string, res: Writable): Promise<void> {
   return new Promise((resolve, reject) => {
-    FilestoreFactory.instance.filestore.getObject(FILE_BUCKET, fileId, (err, result) => {
-      if (err) return reject(err);
-      return resolve(result);
-    });
+    FilestoreFactory.instance.filestore.getObject(
+      ROOT_BUCKET,
+      'files/' + fileId,
+      (err, result) => {
+        if (err) return reject(err);
+        result.pipe(res);
+        result.on('error', reject);
+        result.on('end', resolve);
+      },
+    );
   });
 }
 
-export async function createFile(fileId: string, stream: Stream): Promise<void> {
+export async function uploadFile(fileId: string, req: Stream): Promise<void> {
   return new Promise((resolve, reject) => {
-    FilestoreFactory.instance.filestore.putObject(FILE_BUCKET, fileId, stream, (err, result) => {
-      if (err) return reject(err);
-      console.log(`Finished Uploading to Minio: ${result}.`);
-      return resolve();
-    });
+    FilestoreFactory.instance.filestore.putObject(
+      ROOT_BUCKET,
+      'files/' + fileId,
+      req,
+      (err, result) => {
+        if (err) return reject(err);
+        console.log(`Finished Uploading to Minio: ${result}.`);
+        return resolve();
+      },
+    );
   });
 }
